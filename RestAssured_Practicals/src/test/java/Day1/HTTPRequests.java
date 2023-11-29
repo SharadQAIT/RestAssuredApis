97b8bf4c7c06bbc75e542471bf6e2089120746dd
Sharad Khairnar <sharad.khairnar.sa@gmail.com>
2023-11-29 12:47:19 +0530

    New code changes and rest assured request data code uploaded

--- RestAssured_Practicals/src/test/java/Day1/HTTPRequests.java
+++ RestAssured_Practicals/src/test/java/Day1/HTTPRequests.java
@@ -15,67 +15,75 @@ import java.util.HashMap;
  * 
  * then()
  * validate status code,extract response,extract headers cookies & response body...
+ * 
+ * 
+ * 
  */
 
 public class HTTPRequests {
 
 	int id;
-	
-	@Test(priority=1)
-	void getUser() 
-	{
+
+	@Test(priority = 1)
+	void getUser() {
 		given()
 
-				.when()
-					.get("https://reqres.in/api/users?page=2")
+				.when().get("https://reqres.in/api/users?page=2")
+
 				.then()
-					.statusCode(200)
-					.body("page",equalTo(2))
-					.log().all();
+				// .assertThat()- that assertion, specifying that the expected status code of
+				// the HTTP response should be 200.
+				.statusCode(200).body("page", equalTo(2)).log().all();
+
+		//
 	}
-	
-	@Test(priority=2)
-	void createUser()
-	{
-		
+
+	@Test(priority = 2)
+	void createUser() {
 		/* This is hard coded value is passed by hashmap */
-		
-		HashMap<String, String> data= new HashMap<String, String>();
+
+		HashMap<String, String> data = new HashMap<String, String>();
 		data.put("name", "sharad");
-		data.put("email", "sharad.khairnar.sa@gmail.com");
-		
-		id=given()
-			.contentType("application/json")
-			.body(data)
-		
-		.when()
-			.post("https://reqres.in/api/users")
-			.jsonPath().getInt("id");
-		
-		//	.then()
-		//	.statusCode(201)
-		//	.log().all();
+		data.put("email", "sharad@gmail.com");
+		data.put("age", "30");
+
+		id = given().contentType("application/json").body(data).when().post("https://reqres.in/api/users").jsonPath()
+				.getInt("id");
+
 	}
-	@Test(priority=3, dependsOnMethods = "createUser")
-	void updateUser()
+
+	@Test(priority=3, dependsOnMethods = "createUser") void updateUser() 
 	{
-		
-		/* This is hard coded value is passed by hashmap */
-		
-		HashMap<String, String> data= new HashMap<String, String>();
-		data.put("name", "Sarthak");
-		data.put("email", "sarthak@gmail.com");
-		
+			// This is hard coded value is passed by hashmap 	
+			// dependsOnMethods = Once "CreateUser" method and code successfully executed then update user code run.
+  
+	  HashMap<String, String> data= new HashMap<String, String>(); 
+	  data.put("name","Sarthak");
+	  data.put("email","sarthak@gmail.com");
+	  data.put("age", "35");
+
+	 given() 
+	  .contentType("application/json") 
+	  .body(data)
+	  
+	  .when() 
+	  .put("https://reqres.in/api/users/"+id)
+	  
+	  .then() 
+	  .statusCode(200)
+	  .log().all(); 
+
+	}
+	@Test(priority=4)
+	void deleteUser()
+	{
 		given()
-			.contentType("application/json")
-			.body(data)
 		
 		.when()
-			.put("https://reqres.in/api/users"+id)
-		
+			.delete("https://reqres.in/api/users/"+id)
 		.then()
-			.statusCode(404)
+			.statusCode(204)
 			.log().all();
 	}
-
 }
+
---
+++ RestAssured_Practicals/src/test/java/Day2/DiffWaysToCreatePostRequestBody.java
@@ -0,0 +1,145 @@
+package Day2;
+
+import org.json.JSONObject;
+import org.json.JSONTokener;
+import org.testng.annotations.Test;
+
+import com.google.gson.JsonObject;
+
+import io.restassured.RestAssured;
+import io.restassured.internal.support.FileReader;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+import java.io.File;
+import java.io.Reader;
+import java.util.HashMap;
+
+/* How many ways we can create request body
+ using HashMap
+ using Org.json
+ using POJO (Plain Old Java Object)
+ using external json file
+*/
+
+public class DiffWaysToCreatePostRequestBody {
+
+	
+	//1)  using HashMap post request
+	
+	@Test(priority = 1)
+	void TestRequestBodybyusingHashMap_Login()
+	 {
+	HashMap<String, String> data=new HashMap<String, String>();
+	data.put("email", "sharad.khairnar@solutionanalysts.com");
+	data.put("password", "Admin@123");
+	
+		 
+		
+		given()
+		 	
+		 	.contentType("application/json")
+		 	.body(data)
+		 
+		 .when()
+		 	.post("https://omnistage-api.solutionanalysts.us/auth/login")
+		 
+		 .then()
+		 	.statusCode(200)
+		 	.log().all();
+	 }
+
+	@Test(priority = 2)
+	void TestRequestBodybyusingHashMap_VerifyEmail_OTP()
+	 {
+	HashMap<String, String> data=new HashMap<String, String>();
+	data.put("email", "sharad.khairnar@solutionanalysts.com");
+	data.put("otp", "123456");
+	
+		 
+		
+		given()
+		 	
+		 	.contentType("application/json")
+		 	.body(data)
+		 
+		 .when()
+		 	.post("https://omnistage-api.solutionanalysts.us/auth/otpverify")
+		 
+		 .then()
+		 	.statusCode(200)
+		 	.log().all();
+	 }
+	
+	//2) Post request by  using Org.json
+
+	@Test (priority=3)
+	void orgjson()
+	{
+		JSONObject data=new JSONObject();
+		data.put("email", "sharad.khairnar@solutionanalysts.com");
+		data.put("password", "Admin@123");
+		
+		
+		given()
+	 	
+	 	.contentType("application/json")
+	 	.body(data.toString())
+	 
+	 	.when()
+	 	.post("https://omnistage-api.solutionanalysts.us/auth/login")
+	 
+	 	.then()
+	 	.statusCode(200)
+	 	.log().all();
+	}
+	
+	//3) Post request by  using Pojo class request
+
+	
+	@Test (priority=4)
+	void pojoRequest()
+	{
+		Pojo_PostRequest data=new Pojo_PostRequest();
+		data.setEmail("sharad.khairnar@solutionanalysts.com");
+		data.setPassword("Admin@123");
+		
+		
+		given()
+	 	
+	 	.contentType("application/json")
+	 	.body(data)
+	 
+	 	.when()
+	 	.post("https://omnistage-api.solutionanalysts.us/auth/login")
+	 
+	 	.then()
+	 	.statusCode(200)
+	 	.log().all();
+	}
+	
+	//4) Post request by using external json file
+
+
+	
+	@Test (priority=5)
+	void externalJsonFile()
+	{
+		
+		String jsonFilePath= System.getProperty("user.dir"); 
+		File f=new File(jsonFilePath+"/src/test/resources/JsonDataFile/body.json");
+		
+		given()
+	 	
+	 	.contentType("application/json")
+	 	.body(f)
+	 	.when()
+	 	.post("https://omnistage-api.solutionanalysts.us/auth/login")
+	 
+	 	.then()
+	 	.statusCode(200)
+	 	.log().all();
+	}
+
+}
